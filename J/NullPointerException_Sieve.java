5d6d3bc4e1cd3fade9870497789057be5f07f006
Hawk Weisman <hawk.weisman@gmail.com>
2013-09-02 15:45:17 -0400

    added test class

--- J/NullPointerException_Sieve.java
+++ J/NullPointerException_Sieve.java
@@ -1,25 +1,28 @@
-import java.util.scanner;
+import java.util.Scanner;
 
-public class Sieve(){
+public class NullPointerException_Sieve {
 	
-	final private int MAX_NUM = 50000
-	private boolean primes = new int[50001];
+	private final int MAX_NUM = 50000;
+	private boolean[] primes = new boolean[50001];
 	private int upper, lower;
+	Scanner scan = new Scanner(System.in);
 	
 	/**
 	Constructor for Sieve
 	*/
 	public NullPointerException_Sieve() {
-		Scanner scan = new Scanner(System.in);
 		primes[0] = false;
 		primes[1] = false;
-		for (i = 1; i <= primes.length(); i++){
+		upper = MAX_NUM;
+		lower = 1;
+		for (int i = 1; i <= primes.length; i++){
 			primes[1] = true;
 		}
+	}
 	
-		/**
-		Implements the Sieve of Eratosthenes algorithm
-		*/
+	/**
+	Implements the Sieve of Eratosthenes algorithm
+	*/
 		public void processSieve(){
 
 		int i = 1;
@@ -50,11 +53,11 @@ public class Sieve(){
 	/**
 	Shows the set of sexy pairs
 	*/
-	public void showPrimes(){
+	public void showPrimes() {
 		System.out.printf("Here are all of the sexy prime pairs in the range %d to %d \n", lower, upper);
-		int count;
-		for (i = lower; i <= upper; i++) {
-			if (primes[i] == true && primes[i-6] == true) {
+		int count = 0;
+		for (int i = lower; i <= upper; i++) {
+			if (primes[i] == true && i-6 > 0 && primes[i-6] == true) {
 				System.out.printf("%d and %d \n", i, (i-6));
 				count++;
 			}
@@ -65,9 +68,9 @@ public class Sieve(){
 	/**
 	gets lower and upper boundaries
 	*/
-	public void getBoundaries(){
+	public void getBoundaries() {
 		System.out.println("Please enter a lower boundary and an upper boundary and I will print all of the sexy prime pairs between those boundaries.");
-		while (!(lower <= upper)) {
+		do {
 			while (!(upper < 1) || !(upper > 50000)) {
 				System.out.print("Please enter the upper boundary (must be between 1 and 50000):");
 				upper = scan.nextInt();
@@ -78,6 +81,6 @@ public class Sieve(){
 				lower = scan.nextInt();
 				System.out.println();
 			}
-		}
+		} while (!(lower <= upper));
 	}
 }
---
+++ J/NullPointerException_SieveTest.java
@@ -0,0 +1,9 @@
+public class NullPointerException_SieveTest {
+	public static void main(String argv[]) {
+	NullPointerException_Sieve steve = new NullPointerException_Sieve();
+	steve.processSieve();
+	steve.showPrimes();
+	steve.getBoundaries();
+	steve.showPrimes();
+	}
+}
