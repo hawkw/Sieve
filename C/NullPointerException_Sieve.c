b35474d4c68e6ee12b3ec65c367ccd80ca57b891
pagea <pagea@allegheny.edu>
2013-09-02 18:11:27 -0400

    Update NullPointerException_Sieve.c
    Rewrote sieve algorithm; fixed bug that caused all numbers to be replaced with 0

--- C/NullPointerException_Sieve.c
+++ C/NullPointerException_Sieve.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 typedef struct Bounds Bounds;
 
+//should move function declarations to a .h file
+void processSieve();
+
 struct Bounds {
     int lowBound;
     int highBound;
@@ -13,4 +16,25 @@ void getBoundaries() {
     scanf("%d", &b.lowBound);
     printf("The upper bound: ");
     scanf("%d", &b.highBound);
+}
+
+void processSieve() {
+    //p is the current prime number being evaluated
+    int p = 2;
+    int i, j;
+    //Populate our array with numbers
+    for (i = 0; i < ARRAYSIZE; i++){
+        primes[i] = i; 
+    }
+
+    //iterate through every element
+    for (i = p; p < ARRAYSIZE; i++){
+        //if the element isn't a non-prime, cross off its multiples
+        if (primes[i]){
+            for (j = i + p; j < ARRAYSIZE; j = j + p){
+                primes[j] = 0;
+            }
+        }
+        p++;
+    }
 }
