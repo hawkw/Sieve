1089fb9e75994415ec41116827f3ac2d2da9589b
pagea <pagea@allegheny.edu>
2013-09-03 01:06:09 -0400

    Added required input validation to boundSizeCheck (untested)

--- C/NullPointerException_Sieve.c
+++ C/NullPointerException_Sieve.c
@@ -3,6 +3,7 @@ typedef struct Bounds Bounds;
 
 //should move function declarations to a .h file
 void processSieve();
+int boundSizeCheck(int endpoint);
 
 struct Bounds {
     int lowBound;
@@ -12,10 +13,22 @@ struct Bounds {
 Bounds b = {0,0};
 
 void getBoundaries() {
-    printf("The lower bound: ");
+    printf("Enter the lower bound: ");
     scanf("%d", &b.lowBound);
-    printf("The upper bound: ");
+    boundSizeCheck(b.lowBound);
+    printf("Enter he upper bound: ");
     scanf("%d", &b.highBound);
+    boundSizeCheck(b.highBound);
+}
+
+
+//Make sure input is of reasonable size
+int boundSizeCheck(int low) {
+    if (b.lowBound > 50000 || b.lowbound < 1){
+        printf("Inputs must be below 50000 and above 0.");
+        getBoundaries();
+    }
+    else return 0;
 }
 
 void processSieve() {
